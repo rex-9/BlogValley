4685ae59877a631139c3a7f34839976ddd0520f9
Htet Naing <htetnaing0814@gmail.com>
2022-10-20 15:30:54 +0630

    added tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
 
-  validates :title, presence: true, length: { maximum: 250, too_long: 'Title must not exceed 250 characters' }
+  validates :title, presence: true, length: { in: 3...250, too_short: 'Title must not be shorter than 3 characters.', too_long: 'Title must not exceed 250 characters.' }
   validates :text, presence: true
   validates :comments_counter, :likes_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'user_id'
   has_many :likes, foreign_key: 'user_id'
 
-  validates :name, presence: true
+  validates :name, presence: true, length: { in: 3...50 }
   validates :posts_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def three_recent_posts
